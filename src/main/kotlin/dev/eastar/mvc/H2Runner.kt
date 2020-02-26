863e5265690b1cbaefd7076e85e9a8a9cde1e602
eastarj <eastarj@gmail.com>
2020-02-26 19:54:43 +0900

    http://localhost:8080/h2-console h2 console running jdbc:h2:mem:testdb

--- src/main/kotlin/dev/eastar/mvc/H2Runner.kt
+++ src/main/kotlin/dev/eastar/mvc/H2Runner.kt
@@ -3,6 +3,7 @@ package dev.eastar.mvc
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
+import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.stereotype.Component
 import javax.sql.DataSource
 
@@ -10,6 +11,9 @@ import javax.sql.DataSource
 class H2Runner : ApplicationRunner {
     @Autowired
     lateinit var dataSource: DataSource
+
+    @Autowired
+    lateinit var jdbcTemplate: JdbcTemplate
 
     override fun run(args: ApplicationArguments?) {
         dataSource.connection.let {
@@ -19,8 +23,10 @@ class H2Runner : ApplicationRunner {
             val ststement = it.createStatement()
             val sql = "CREATE TABLE USER (ID INTEGER NOT NULL, name VARCHAR(255), PRIMARY KEY (id))"
             ststement.executeUpdate(sql)
-
         }
+
+        jdbcTemplate.execute("INSERT INTO USER VALUES (1, 'eastar')")
+
     }
 
 
