bc0605a9aece1c6729ccd91bea9cc1bdcbcd48ce
eastarj <eastarj@gmail.com>
2020-03-11 20:08:56 +0900

    only web

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,10 +20,9 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	//implementation("org.springframework.boot:spring-boot-starter-data-readis")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	//{
-	//	exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
-	//}
 }
 
 tasks.withType<Test> {
